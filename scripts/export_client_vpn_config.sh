039ed9047fa9cf6b3985f61ce12dd8711db94d1f
SemionK <semion.k@icloud.com>
2022-06-30 09:54:53 -0400

    adde region and profile

--- scripts/export_client_vpn_config.sh
+++ scripts/export_client_vpn_config.sh
@@ -5,7 +5,7 @@ KEY_SAVE_FOLDER_PATH=$PKI_FOLDER_NAME/$KEY_SAVE_FOLDER
 FULL_CLIENT_CERTIFICATE_NAME=$CLIENT_CERT_NAME.$TENANT_NAME
 CLIENT_CERTIFICATE=$CLIENT_CERT_NAME.$CERT_ISSUER
 
-aws ec2 export-client-vpn-client-configuration --client-vpn-endpoint-id $CLIENT_VPN_ID --output text > $FULL_CLIENT_CERTIFICATE_NAME.ovpn
+aws ec2 export-client-vpn-client-configuration --client-vpn-endpoint-id $CLIENT_VPN_ID --output text > $FULL_CLIENT_CERTIFICATE_NAME.ovpn --region $REGION --profile $AWSCLIPROFILE
 
 sed -i "s/"$CLIENT_VPN_ID"/"$TENANT_NAME.$CLIENT_VPN_ID"/g" $FULL_CLIENT_CERTIFICATE_NAME.ovpn
 echo "<cert>" >> $FULL_CLIENT_CERTIFICATE_NAME.ovpn
